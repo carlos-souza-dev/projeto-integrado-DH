a472fffd9a41bf95b4b0b3c83a3d564306870e66
Camila <camilacslopes@gmail.com>
2020-05-24 13:53:48 -0300

    Editando e Excluindo Contatos

--- controllers/contatosUteisController.js
+++ controllers/contatosUteisController.js
@@ -5,21 +5,57 @@ const contatosUteisController = {
 
     exibir: async (req, res) => {
         console.log(ContatosUteis);
-        let docs = await ContatosUteis.findAll();
+        let contato = await ContatosUteis.findAll();
 
-        return res.render('contatosUteis', {docs});
+        return res.render('contatosUteis', {contato});
     },
 
     store: async (req, res) => {
 
-        const {foto, nome, descricao, numero, email} = req.body;
         const [files] = req.files;
+        const {nome, descricao, numero, email} = req.body;
+        
 
         const resultado = await ContatosUteis.create(
-            {foto,nome, descricao, numero, email: `/docs/${files.filename}`}
+            {foto: `/img/${files.filename}`, nome, descricao, numero, email}
         );
 
         return res.redirect("/contatosUteis");
+
+    },
+    update: async (req, res) => {
+        const {id} = req.params;
+        const [files] = req.files;
+        const {nome, descricao, numero, email} = req.body;
+
+        if ([files] == "") {
+            const contatos = await ContatosUteis.update({
+                foto: `/img/${files.filename}`,
+                nome,
+                descricao,
+                numero,
+                email
+            }, {
+                where: {
+                    id: id
+                }
+            })
+
+        } else {
+            const contatos = await ContatosUteis.update({
+                
+                foto: `/img/${files.filename}`,
+                nome,
+                descricao,
+                numero,
+                email
+            }, {
+                where: {
+                    id: id
+                }
+            })
+        }
+        return res.redirect('/contatosUteis');
 
     },
 
